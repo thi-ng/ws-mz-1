6500b201a592675db24e6d0e831cc08708217095
Karsten Schmidt <k@postspectacular.com>
2016-06-12 14:52:55 +0200

    update cljs example

--- src-cljs/day2/ex03/core.cljs
+++ src-cljs/day2/ex03/core.cljs
@@ -3,13 +3,35 @@
    [reagent.ratom :refer [reaction]]
    [cljs-log.core :refer [debug info]])
   (:require
-   [reagent.core :as reagent]))
+   [reagent.core :as reagent]
+   [clojure.string :as str]))
 
-(defonce app (atom {}))
+(defonce app
+  (atom {:title "hey"
+         :body ""}))
+
+(defn set-key
+  [key]
+  (fn [e]
+    (swap! app assoc key (-> e .-target .-value))))
 
 (defn app-component
   []
-  [:div [:h1 "Hallo Mainzer!"]])
+  (let [title (reaction (:title @app))
+        body  (reaction (str/upper-case (:title @app)))]
+    (fn []
+      [:div
+       [:h2 "Hallo"]
+       [:label "Titel"
+        [:input
+         {:type "text"
+          :default-value @title
+          :on-change (set-key :title)}]]
+       [:label "Inhalt"
+        [:textarea
+         {:cols 60
+          :rows 20
+          :value @body}]]])))
 
 (defn main
   []
